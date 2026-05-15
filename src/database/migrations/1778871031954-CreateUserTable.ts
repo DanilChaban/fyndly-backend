d682aa84b32b779d6f66c7a6b760dc2cbb12c207
Danylo Chaban <krigerblood@gmail.com>
2026-05-16 00:47:07 +0300

    feat: add "remember me" functionality to authentication system
    - Added `rememberMe` field in Login DTO, allowing users to opt for extended session duration.
    - Updated JWT creation logic to set token expiration dynamically based on `rememberMe` value.
    - Enhanced cookie management to handle varying expiration times and ensure consistent naming with `ACCESS_TOKEN`.
    - Refactored logout method to utilize centralized cookie clearing logic.

--- src/database/migrations/1778871031954-CreateUserTable.ts
+++ src/database/migrations/1778871031954-CreateUserTable.ts
@@ -1,14 +1,13 @@
-import { MigrationInterface, QueryRunner } from "typeorm";
+import { MigrationInterface, QueryRunner } from 'typeorm';
 
 export class CreateUserTable1778871031954 implements MigrationInterface {
-    name = 'CreateUserTable1778871031954'
+  name = 'CreateUserTable1778871031954';
 
-    public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "user" RENAME COLUMN "name" TO "username"`);
-    }
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`ALTER TABLE "user" RENAME COLUMN "name" TO "username"`);
+  }
 
-    public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "user" RENAME COLUMN "username" TO "name"`);
-    }
-
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`ALTER TABLE "user" RENAME COLUMN "username" TO "name"`);
+  }
 }
