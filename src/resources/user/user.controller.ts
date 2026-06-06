11a0dd0bc9e329ead3e9d411598f22d245c6750b
Danylo Chaban <krigerblood@gmail.com>
2026-06-07 00:59:18 +0300

    feat: add user profile update functionality
    - Added `updateUser` method in `UserService` and new `UpdateUserDto` for request validation.
    - Updated `UserEntity` with new optional fields: `firstName`, `lastName`, `phoneNumber`, `country`, `city`, `avatar`, and `coverImage`.
    - Added `PATCH /user` endpoint in `UserController` for updating user profiles.

--- src/resources/user/user.controller.ts
+++ src/resources/user/user.controller.ts
@@ -1,16 +1,22 @@
-import { Controller, Get, UseGuards } from '@nestjs/common';
+import { Body, Controller, Get, Patch, UseGuards } from '@nestjs/common';
 import { User } from '@core/decorators/user.decorator';
 import { JwtAuthGuard } from '@core/guards/jwt-auth.guard';
 import { UserEntity } from '@user/entities/user.entity';
 import { UserService } from '@user/user.service';
+import { UpdateUserDto } from '@user/dto/update-user.dto';
 
 @UseGuards(JwtAuthGuard)
 @Controller('user')
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
-  @Get('me')
-  async me(@User() user: UserEntity): Promise<UserEntity> {
-    return await this.userService.me(user.id);
+  @Get()
+  async user(@User() user: UserEntity): Promise<UserEntity> {
+    return await this.userService.user(user.id);
+  }
+
+  @Patch()
+  async updateUser(@User() user: UserEntity, @Body() updateUserDto: UpdateUserDto): Promise<UserEntity> {
+    return await this.userService.updateUser(user.id, updateUserDto);
   }
 }
